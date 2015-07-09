181e56ba40325223a546c74840c32d43871cce9d
Khaled Hosny <khaledhosny@eglug.org>
2015-07-09 05:44:53 +0200

    Add some metadata

--- tools/build.py
+++ tools/build.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python2
+# encoding: utf-8
+
 import sys
+from datetime import datetime
 try:
     from sortsmill import ffcompat as fontforge
 except ImportError:
@@ -19,6 +23,24 @@ for glyph in latin.glyphs():
 
 arabic.mergeFonts(latin)
 
+# Set metadata
 arabic.version = sys.argv[3]
-arabic.copyright += " Portions " + latin.copyright[0].lower() + latin.copyright[1:]
+years = datetime.now().year == 2015 and 2015 or "2015-%s" % datetime.now().year
+
+arabic.copyright = ". ".join(["Portions copyright © %s, Khaled Hosny (<khaledhosny@eglug.org>)",
+                              "Portions " + latin.copyright[0].lower() + latin.copyright[1:].replace("(c)", "©")])
+arabic.copyright = arabic.copyright % years
+
+en = "English (US)"
+arabic.appendSFNTName(en, "Designer", "Abdoulla Aref")
+arabic.appendSFNTName(en, "License URL", "http://scripts.sil.org/OFL")
+arabic.appendSFNTName(en, "License", 'This Font Software is licensed under the SIL Open Font License, Version 1.1. \
+This Font Software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR \
+CONDITIONS OF ANY KIND, either express or implied. See the SIL Open Font License \
+for the specific language, permissions and limitations governing your use of \
+this Font Software.')
+arabic.appendSFNTName(en, "Descriptor", "Aref Ruqaa is an Arabic typeface that aspires to capture the essence of \
+the classical Ruqaa calligraphic style.")
+arabic.appendSFNTName(en, "Sample Text", "الخط هندسة روحانية ظهرت بآلة جسمانية")
+
 arabic.generate(sys.argv[2], flags=("round", "opentype"))
