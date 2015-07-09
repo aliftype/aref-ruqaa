67b0eecb5970b3b0e3d928c29313add77ffcefe4
Khaled Hosny <khaledhosny@eglug.org>
2015-07-09 04:24:23 +0200

    Be more selective on what glyphs to merge
    * Only Latin glyphs from Euler, I don’t want the Greek.
    * Only Arabic glyphs from Aref, I don’t want the existing punctuation.

--- tools/build.py
+++ tools/build.py
@@ -8,6 +8,15 @@ arabic = fontforge.open(sys.argv[1])
 arabic.em = 1000
 
 latin = fontforge.open(sys.argv[1].replace("arefruqaa", "eulertext"))
+
+for glyph in arabic.glyphs():
+    if glyph.unicode < 0x0600 and glyph.unicode != -1 and glyph.unicode != ord(" "):
+        arabic.removeGlyph(glyph)
+
+for glyph in latin.glyphs():
+    if glyph.unicode > ord("~") or glyph.unicode == -1:
+        latin.removeGlyph(glyph)
+
 arabic.mergeFonts(latin)
 
 arabic.version = sys.argv[3]
