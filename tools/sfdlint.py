7c55cd65608e1c4ac823e7585d82ecc4a54b19ca
Khaled Hosny <khaledhosny@eglug.org>
2015-09-12 21:17:20 +0200

    Fix more negative right side bearings

--- tools/sfdlint.py
+++ tools/sfdlint.py
@@ -4,17 +4,30 @@ import sys
 font = fontforge.open(sys.argv[1])
 
 bad_glyph_classes = {}
+bad_side_bearings = {}
+
 for glyph in font.glyphs():
     if glyph.glyphclass not in ("mark", "baseglyph"):
-        if glyph.glyphclass in bad_glyph_classes:
-            bad_glyph_classes[glyph.glyphclass].append(glyph.glyphname)
-        else:
-            bad_glyph_classes[glyph.glyphclass] = [glyph.glyphname]
+        if glyph.glyphclass not in bad_glyph_classes:
+            bad_glyph_classes[glyph.glyphclass] = []
+        bad_glyph_classes[glyph.glyphclass].append(glyph.glyphname)
+    if ".init" in glyph.glyphname or ".isol" in glyph.glyphname:
+        if "arKaf.init" not in glyph.glyphname:
+            if glyph.right_side_bearing <= 0:
+                if "left" not in bad_side_bearings:
+                    bad_side_bearings["left"] = []
+                bad_side_bearings["left"].append(glyph.glyphname)
 
 if bad_glyph_classes:
     from pprint import pformat
     print >> sys.stderr, "Some glyphs have bad glyph class:"
     print >> sys.stderr, pformat(bad_glyph_classes)
+    sys.exit(1)
+
+if bad_side_bearings:
+    from pprint import pformat
+    print >> sys.stderr, "Some glyphs have bad side bearings:"
+    print >> sys.stderr, pformat(bad_side_bearings)
     sys.exit(1)
 
 with open(sys.argv[2], "w") as log:
