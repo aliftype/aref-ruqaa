dc7844bb662fa05dd0d4e29e7b39f5a621df6dfe
Kalapi Gajjar <kalapi.gajjar@gmail.com>
2016-09-03 12:54:27 +0530

    Fixes spacing in the Latin sources using both auto and manual spacing

---
+++ sources/Glyphs/eulertext-regular_autospace.py
@@ -0,0 +1,33 @@
+# CATEGORIES / CATEGORIAS
+# Script, Category, Subcategory, value, referenceGlyph, filter
+# Declare the general and specific / declarar de general a particular
+
+*,Letter,Uppercase,1.35,H,*,
+*,Letter,Lowercase,1.05,x,*,
+*,Letter,Superscript,0.7,ordfeminine,*,
+
+# Numbers / Numeros
+*,Number,Decimal Digit,1.3,one,*,
+*,Number,Fraction,1.3,*,*,
+*,Number,*,0.8,*,superior,
+
+# Punctuation / Puntuacionyotros
+# *,Punctuation,*,1.5,*,quote,
+*,Punctuation,Other,1.4,*,*,
+*,Punctuation,Parenthesis,1.5,*,*,
+*,Punctuation,Quote,1.2,*,*,
+*,Punctuation,Quote,1.35,*,guil,
+*,Punctuation,Dash,1,*,*,
+*,Punctuation,*,1,*,slash,
+*,Punctuation,*,1.2,*,*,
+
+# Symbols
+*,Symbol,Currency,1.6,*,*,
+*,Symbol,Math,2,*,*,
+*,Symbol,*,1.5,*,*,
+*,Mark,*,1,*,*,
+
+# Greek
+greek,Letter,Uppercase,1.3,H,*,
+greek,Letter,Lowercase,1.05,x,*,
+
