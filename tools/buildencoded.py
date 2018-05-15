c8dcbb87f8cf527c2158ab559c58208f46588c22
Khaled Hosny <khaledhosny@eglug.org>
2018-05-16 00:36:06 +0200

    Don’t require the font to have placeholder glyphs
    Just create them if they are missing.

--- tools/buildencoded.py
+++ tools/buildencoded.py
@@ -2,6 +2,7 @@ import sys
 
 def parse(fea):
     subs = {}
+    baseGlyphs = None
     for statement in fea.statements:
         if getattr(statement, "name", None) in ("isol", "ccmp"):
             for substatement in statement.statements:
@@ -13,28 +14,34 @@ def parse(fea):
                 elif hasattr(substatement, "glyph"):
                     # Multiple
                     subs[substatement.glyph] = substatement.replacement
+        elif getattr(statement, "name", None) == "GDEF":
+            for substatement in statement.statements:
+                if hasattr(substatement, "baseGlyphs"):
+                    baseGlyphs = substatement.baseGlyphs
+                    if hasattr(baseGlyphs, "glyphclass"):
+                        baseGlyphs = baseGlyphs.glyphclass
 
-    return subs
+    return subs, baseGlyphs
 
 def build(font, features):
-    subs = parse(features)
+    subs, baseGlyphs = parse(features)
 
     temp_glyph = font.createChar(-1, "TempXXX")
 
-    for glyph in font.glyphs():
-        if glyph.glyphname in subs:
-            names = subs[glyph.glyphname]
-            # build the composite on a temp glyph to prevent FontForge from
-            # using its built-in knowledge about components of some encoded
-            # glyphs.
-            temp_glyph.clear()
-            temp_glyph.addReference(names[0])
-            if len(names) > 1:
-                for name in names[1:]:
-                    temp_glyph.appendAccent(name)
-                temp_glyph.build()
-            glyph.clear()
-            glyph.references = temp_glyph.references
-            glyph.useRefsMetrics(names[0])
-            glyph.color = 0xff0000
+    for base in subs:
+        names = subs[base]
+        glyph = font.createMappedChar(base)
+        # build the composite on a temp glyph to prevent FontForge from using
+        # its built-in knowledge about components of some encoded glyphs.
+        temp_glyph.clear()
+        temp_glyph.addReference(names[0])
+        for name in names[1:]:
+            temp_glyph.appendAccent(name)
+        temp_glyph.build()
+        glyph.clear()
+        glyph.references = temp_glyph.references
+        glyph.useRefsMetrics(names[0])
+        glyph.color = 0xff0000
+        baseGlyphs.glyphs.append(base)
+
     font.removeGlyph(temp_glyph)
