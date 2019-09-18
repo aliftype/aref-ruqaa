65b0add5d77dc572dda7ecb0f605f71cf46768ec
Khaled Hosny <khaledhosny@eglug.org>
2019-09-18 17:45:03 +0200

    Sync bold anchors with regular

--- tools/sync-anchors.py
+++ tools/sync-anchors.py
@@ -4,21 +4,19 @@ import sys
 font1 = fontforge.open(sys.argv[1])
 font2 = fontforge.open(sys.argv[2])
 
-anchor_names = sys.argv[3:]
-
 for glyph1 in font1.glyphs():
-    anchors1 = sorted(glyph1.anchorPoints)
     glyph2 = font2[glyph1.glyphname]
+
+    anchors1 = sorted(glyph1.anchorPoints)
     anchors2 = sorted(glyph2.anchorPoints)
     anchors3 = []
-    for i, anchor in enumerate(anchors1):
-        if anchor[0] in anchor_names:
-            anchors3.append(anchor)
-        elif i >= len(anchors2):
-            anchors3.append(anchor)
-        else:
-            anchors3.append(anchors2[i])
-    if anchors3 != anchors2:
-        glyph2.anchorPoints = anchors3
 
-font2.save()
+    anchors2_names = [a[0] for a in anchors2]
+    for i, anchor in enumerate(anchors1):
+        if anchor[0] not in anchors2_names:
+            anchors3.append(anchor)
+    anchors3 = set(anchors2 + anchors3)
+    if anchors3 != set(anchors2):
+        glyph2.anchorPoints = tuple(anchors3)
+
+font2.save(sys.argv[2])
