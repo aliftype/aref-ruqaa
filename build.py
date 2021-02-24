1dacf7509a3facfba1c50a569fc93e4b38ae6ce2
Khaled Hosny <khaled@aliftype.com>
2021-02-24 17:33:20 +0200

    Simplify build

--- build.py
+++ build.py
@@ -3,51 +3,11 @@ import io
 
 from datetime import datetime
 
-from fontTools.ttLib import TTFont
-from fontTools.feaLib import ast, parser, builder
-
 from sfdLib.parser import SFDParser
 
 from ufo2ft import compileTTF
 
 from ufoLib2 import Font
-
-
-def parse_features(font):
-    fea = parser.Parser(io.StringIO(font.features.text), font.glyphOrder).parse()
-
-    # Drop script and language statements from GPOS features (which are
-    # generated from sources), so that they inherit from the global
-    # languagesytem’s set in the feature file. This way I don’t have to
-    # manually repeat them.
-    langsys = []
-    statements = []
-    for statement in fea.statements:
-        name = getattr(statement, "name", "")
-        if isinstance(statement, ast.LanguageSystemStatement):
-            langsys.append(statement)
-            continue
-        if name in ("curs", "mark", "mkmk"):
-            scripts = []
-            languages = []
-            substatements = []
-            for substatement in statement.statements:
-                if isinstance(substatement, ast.ScriptStatement):
-                    scripts.append(substatement.script)
-                elif isinstance(substatement, ast.LanguageStatement):
-                    languages.append(substatement.language)
-                else:
-                    substatements.append(substatement)
-            # There must be one script and one language statement, otherwise
-            # the lookups will be duplicated.
-            assert len(scripts) == 1, statement
-            assert len(languages) == 1, statement
-            statement.statements = substatements
-        statements.append(statement)
-
-    fea.statements = langsys + statements
-
-    return fea
 
 
 def build(args):
@@ -65,17 +25,12 @@ def build(args):
     with open(args.feature_file) as feature_file:
         font.features.text += feature_file.read()
 
-    # Add Arabic GPOS features from the SFD file.
-    fea = parse_features(font)
-
     # Set metadata
     info = font.info
 
     major, minor = args.version.split(".")
     info.versionMajor, info.versionMinor = int(major), int(minor)
     info.copyright = info.copyright.format(year=datetime.now().year)
-
-    font.features.text = str(fea)
 
     compileTTF(font, inplace=True).save(args.out_file)
 
