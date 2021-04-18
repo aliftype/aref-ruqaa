47e58172e84d5fb20bc24e2d7e9df43bc4c1bd74
Khaled Hosny <khaled@aliftype.com>
2021-04-18 19:37:43 +0200

    Use UFO anchors
    Preparing for switching away from FontForge.

--- build.py
+++ build.py
@@ -9,17 +9,74 @@ from ufo2ft import compileTTF
 
 from ufoLib2 import Font
 
+from fontTools.misc.fixedTools import otRound
+
+
+def _dumpAnchor(anchor):
+    if not anchor:
+        return "<anchor NULL>"
+    return f"<anchor {otRound(anchor.x)} {otRound(anchor.y)}>"
+
 
 def build(args):
     font = Font(validate=False)
     SFDParser(
         args.file,
         font,
-        ignore_uvs=False,
-        ufo_anchors=False,
-        ufo_kerning=False,
-        minimal=True,
+        ufo_anchors=True,
+        ufo_kerning=True,
+        minimal=False,
     ).parse()
+
+    lookups = {}
+    lines = []
+
+    for glyph in font:
+        for anchor in glyph.anchors:
+            if anchor.name in ("entry", "exit"):
+                lookups.setdefault("curs", {}).setdefault(glyph.name, [None, None])
+                lookups["curs"][glyph.name][0 if anchor.name == "entry" else 1] = anchor
+            else:
+                name = anchor.name
+                if name.startswith("_"):
+                    name = name[1:]
+                lookup = name.split(".")[0].title()
+                if name.endswith(".mkmk"):
+                    lookup = f"mkmk{lookup}"
+                else:
+                    lookup = f"mark{lookup}"
+                lookups.setdefault(lookup, {}).setdefault(glyph.name, []).append(anchor)
+
+    for lookup in sorted(lookups):
+        lines.append(f"lookup {lookup} {{")
+        if lookup == "curs":
+            lines.append("lookupflag RightToLeft IgnoreMarks;")
+            for glyph, (entry, exit_) in lookups[lookup].items():
+                lines.append(f"pos cursive {glyph} {_dumpAnchor(entry)} {_dumpAnchor(exit_)};")
+        else:
+            type_ = "base"
+            if lookup.startswith("mkmk"):
+                type_ = "mark"
+                glyphs = " ".join([g for g in lookups[lookup].keys()])
+                lines.append(f"lookupflag UseMarkFilteringSet [{glyphs}];")
+            for glyph, anchors in lookups[lookup].items():
+                for anchor in anchors:
+                    if not anchor.name.startswith("_"):
+                        continue
+                    lines.append(f"markClass {glyph} {_dumpAnchor(anchor)} @{anchor.name[1:]};")
+            for glyph, anchors in lookups[lookup].items():
+                if all([a.name.startswith("_") for a in anchors]):
+                    continue
+                line = f"pos {type_} {glyph} "
+                for anchor in anchors:
+                    if anchor.name.startswith("_"):
+                        continue
+                    line += f"{_dumpAnchor(anchor)} mark @{anchor.name} "
+                line += ";"
+                lines.append(line)
+        lines.append(f"}} {lookup};")
+
+    font.features.text += "\n".join(lines)
 
     # Read external feature file.
     with open(args.feature_file) as feature_file:
